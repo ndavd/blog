7206faac1ef371e238f28b6c84ef2d889a599619
Nuno David <ndavidq0@gmail.com>
2025-02-09 21:41:25 +0000

    fix: use relative paths everywhere so that it supports any base route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeMathjax from 'rehype-mathjax';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://ndavd.eth.limo',
-  integrations: [react()],
+  integrations: [],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
---
+++ src/content/test.md
@@ -0,0 +1,7 @@
+---
+title: 'test post'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: 'Jul 22 2022'
+---
+
+### test
