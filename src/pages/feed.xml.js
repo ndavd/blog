d90f8f439fb1830ce88c565666ca453b5864e060
Nuno David <ndavidq0@gmail.com>
2025-02-16 13:55:21 +0000

    feat: add proper rss feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('blog');
@@ -9,8 +13,11 @@ export async function GET(context) {
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
-      ...post.data,
       link: `/${post.id}`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+      ...post.data,
     })),
   });
 }
